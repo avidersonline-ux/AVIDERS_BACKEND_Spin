e795c176c3b59a91b9b1cd1ae336809e861eaa41
avidersonline-ux <avidersonline@gmail.com>
2025-11-26 21:58:25 +0530

    Delete BACKUP directory

--- BACKUP/modules/spinwheel/models/SpinHistory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SpinHistorySchema = new mongoose.Schema({
-  uid: { type: String, required: true },
-  reward: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
-}, { timestamps: true });
-
-module.exports = mongoose.model("SpinHistory", SpinHistorySchema);
--- BACKUP/modules/spinwheel/models/SpinReward.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SpinRewardSchema = new mongoose.Schema({
-  label: { type: String, required: true },
-  value: { type: Number, required: true },
-  chance: { type: Number, required: true },
-});
-
-module.exports = mongoose.model("SpinReward", SpinRewardSchema);
