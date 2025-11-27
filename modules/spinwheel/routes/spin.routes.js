c6bc35e1525b286ef65bc99cb578c3532cc9aab6
avidersonline-ux <avidersonline@gmail.com>
2025-11-27 17:34:09 +0530

    Update spin.routes.js

--- modules/spinwheel/routes/spin.routes.js
+++ modules/spinwheel/routes/spin.routes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const controller = require("../controllers/spin.controller");
+const adsController = require("../controllers/ads.controller");
 
+// Get current spin status (free spin, bonus spins, coins, etc.)
 router.get("/status", controller.spinStatus);
+
+// Spin the wheel
 router.post("/spin", controller.spinNow);
 
-module.exports = router;
+// Watch an ad → earn 1 extra spin (max 10/day)
+router.post("/watch-ad", adsController.watchAd);
 
+module.exports = router;
