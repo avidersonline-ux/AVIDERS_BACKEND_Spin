6de2c3f3069b779c2bba1e7e7d86f466b7d4667d
avidersonline-ux <avidersonline@gmail.com>
2025-11-28 15:01:26 +0530

    Update rewardEngine.js

--- modules/spinwheel-service/utils/rewardEngine.js
+++ modules/spinwheel-service/utils/rewardEngine.js
@@ -3,26 +3,23 @@ const path = require("path");
 
 function loadConfig() {
   const filePath = path.join(__dirname, "../config/rewards.config.json");
-  const raw = fs.readFileSync(filePath, "utf8");
-  return JSON.parse(raw);
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
 }
 
 function pickReward() {
-  const config = loadConfig();
-  const sectors = config.sectors;
+  const { rewards } = loadConfig();
 
-  const total = sectors.reduce((sum, s) => sum + (s.chance || 0), 0);
-  const rand = Math.random() * total;
+  const totalProb = rewards.reduce((sum, r) => sum + r.probability, 0);
+  const rand = Math.random() * totalProb;
 
   let acc = 0;
-  for (let i = 0; i < sectors.length; i++) {
-    acc += sectors[i].chance;
+  for (let i = 0; i < rewards.length; i++) {
+    acc += rewards[i].probability;
     if (rand <= acc) {
-      return { sectorIndex: i, reward: sectors[i] };
+      return { index: i, reward: rewards[i] };
     }
   }
-
-  return { sectorIndex: 0, reward: sectors[0] };
+  return { index: 0, reward: rewards[0] };
 }
 
 module.exports = { pickReward, loadConfig };
