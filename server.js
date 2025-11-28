af755bfe938d3c5eaafe94b7df5f0e9bdf6e1164
avidersonline-ux <avidersonline@gmail.com>
2025-11-28 22:23:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const connectMongo = require("./config/mongo");
 
-connectMongo();
+const connectMongo = require("./config/mongo");
+const connectSpinMongo = require("./modules/spinwheel-service/config/mongo.spin");
+
+// connect BOTH databases
+connectMongo();      
+connectSpinMongo();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/spin", require("./routes/spinRoutes"));
+app.use("/api/spin", require("./modules/spinwheel-service/routes/spin.routes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Spin API running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
