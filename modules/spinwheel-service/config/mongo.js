f8dddac998ee1be4b553e09b5aa719d4785dd4f5
avidersonline-ux <avidersonline@gmail.com>
2025-11-28 22:22:06 +0530

    Update mongo.js

--- modules/spinwheel-service/config/mongo.js
+++ modules/spinwheel-service/config/mongo.js
@@ -1,12 +1,13 @@
+// config/mongo.js
+
 const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_SPIN);
-
-    console.log("✅ Spin DB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("✅ Main MongoDB Connected");
   } catch (err) {
-    console.error("❌ Spin DB Error:", err);
+    console.error("❌ Main MongoDB Error:", err.message);
     process.exit(1);
   }
 };
