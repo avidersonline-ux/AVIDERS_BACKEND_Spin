0afc205dae705bea1b558e645e49889c6eee4e21
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:34:39 +0530

    Update cashback.service.js

--- modules/cashback/cashback.service.js
+++ modules/cashback/cashback.service.js
@@ -1,1 +1,30 @@
+const walletService = require('../wallet/wallet.service');
+const { SOURCES } = require('../wallet/transaction.types');
+
+class CashbackService {
+  /**
+   * Process cashback for a product purchase
+   */
+  async processProductPurchase(userId, orderId, purchaseAmount, cashbackPercentage = 5) {
+    const cashbackAmount = Math.floor((purchaseAmount * cashbackPercentage) / 100);
+
+    if (cashbackAmount > 0) {
+      return await walletService.credit(
+        userId,
+        cashbackAmount,
+        SOURCES.CASHBACK,
+        `cb_${orderId}`,
+        { orderId, purchaseAmount, cashbackPercentage }
+      );
+    }
+    return null;
+  }
+
+  async getCashbackHistory(userId) {
+    const Transaction = require('../../models/Transaction');
+    return await Transaction.find({ userId, source: SOURCES.CASHBACK }).sort({ createdAt: -1 });
+  }
+}
+
+module.exports = new CashbackService();
 
