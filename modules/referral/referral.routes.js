506fc9fdf11b149627dbd315b5e6869ebba99dc6
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:34:58 +0530

    Update referral.routes.js

--- modules/referral/referral.routes.js
+++ modules/referral/referral.routes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const referralService = require('./referral.service');
+const catchAsync = require('../../utils/catchAsync');
+
+router.post('/bonus-sync', catchAsync(async (req, res) => {
+  const { referrerUid, newUserUid, referralCode } = req.body;
+  const result = await referralService.creditReferralBonus(referrerUid, newUserUid, referralCode);
+  res.json({ success: true, ...result });
+}));
+
+module.exports = router;
 
