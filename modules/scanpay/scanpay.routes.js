c55d04b15f7227290960eeefca11ee3ba728a77c
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:35:34 +0530

    Update scanpay.routes.js

--- modules/scanpay/scanpay.routes.js
+++ modules/scanpay/scanpay.routes.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const scanpayService = require('./scanpay.service');
+
+router.post('/pay', async (req, res) => {
+  try {
+    const { uid, merchantId, amount, transactionId } = req.body;
+    const transaction = await scanpayService.processScanAndPay(uid, merchantId, amount, transactionId);
+    res.json({ success: true, transaction });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+module.exports = router;
 
