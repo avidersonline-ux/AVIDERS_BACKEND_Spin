73d3779385c448f05404b743f8f79ef477bb96b1
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:36:56 +0530

    Update transaction.types.js

--- modules/wallet/transaction.types.js
+++ modules/wallet/transaction.types.js
@@ -1,1 +1,19 @@
+module.exports = {
+  CREDIT: 'CREDIT',
+  DEBIT: 'DEBIT',
+
+  SOURCES: {
+    SPIN: 'SPIN',
+    REFERRAL: 'REFERRAL',
+    CASHBACK: 'CASHBACK',
+    SUBSCRIPTION: 'SUBSCRIPTION',
+    MANUAL: 'MANUAL',
+    SPEND: 'SPEND'
+  },
+
+  WALLET_STATUS: {
+    ACTIVE: 'active',
+    FROZEN: 'frozen'
+  }
+};
 
