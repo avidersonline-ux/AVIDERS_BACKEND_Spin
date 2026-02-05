17af2a2a613a5a5757548295029a43902d7fdc2c
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:34:20 +0530

    Update cashback.routes.js

--- modules/cashback/cashback.routes.js
+++ modules/cashback/cashback.routes.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const cashbackService = require('./cashback.service');
+
+router.post('/process', async (req, res) => {
+  try {
+    const { uid, orderId, amount } = req.body;
+    const transaction = await cashbackService.processProductPurchase(uid, orderId, amount);
+    res.json({ success: true, transaction });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+module.exports = router;
 
