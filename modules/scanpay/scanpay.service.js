3193684fc86947f931e53e47098a5f9c6c2ff107
AVIDERS <avidersonline@gmail.com>
2026-02-06 02:35:51 +0530

    Update scanpay.service.js

--- modules/scanpay/scanpay.service.js
+++ modules/scanpay/scanpay.service.js
@@ -1,1 +1,25 @@
+const walletService = require('../wallet/wallet.service');
+const { SOURCES } = require('../wallet/transaction.types');
+
+class ScanPayService {
+  /**
+   * Process a payment made via QR scan at a merchant
+   */
+  async processScanAndPay(userId, merchantId, amount, transactionId) {
+    // Deduct from wallet
+    return await walletService.debit(
+      userId,
+      amount,
+      SOURCES.SPEND,
+      `scan_${transactionId}`,
+      {
+        merchantId,
+        type: 'SCAN_PAY',
+        description: `Payment to merchant ${merchantId}`
+      }
+    );
+  }
+}
+
+module.exports = new ScanPayService();
 
