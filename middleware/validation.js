8ca983c0b5dd87c0d385b8641d24391c2c7be6e9
AVIDERS <avidersonline@gmail.com>
2026-02-06 13:37:37 +0530

    Update validation.js

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,1 +1,21 @@
+const Joi = require('joi');
+const { AppError } = require('../utils/errorHandler');
+
+/**
+ * Higher-order function to validate request body against a Joi schema
+ */
+const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      const errorMessage = error.details.map(detail => detail.message).join(', ');
+      return next(new AppError(errorMessage, 400));
+    }
+    next();
+  };
+};
+
+module.exports = {
+  validateRequest
+};
 
