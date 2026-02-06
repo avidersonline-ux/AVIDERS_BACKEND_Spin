ac025b16b500587076ebdb0fd89e1de7cda72943
AVIDERS <avidersonline@gmail.com>
2026-02-06 14:29:00 +0530

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,1 +1,28 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  console.error('❌ ERROR ❌', err);
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+  });
+};
+
+module.exports = {
+  AppError,
+  globalErrorHandler,
+};
 
