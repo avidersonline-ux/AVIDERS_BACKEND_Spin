3b235af44a78671badd0e365bbfd3c638c0e910e
AVIDERS <avidersonline@gmail.com>
2026-02-06 13:06:54 +0530

    Update wallet.routes.js

--- modules/wallet/wallet.routes.js
+++ modules/wallet/wallet.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const walletController = require('./wallet.controller');
 const catchAsync = require('../../utils/catchAsync');
 
-router.post('/balance', catchAsync(walletController.getBalance));
-router.post('/history', catchAsync(walletController.getHistory));
-router.post('/spend', catchAsync(walletController.spend));
-router.post('/admin/credit', catchAsync(walletController.adminCredit));
+router.post('/balance', catchAsync((req, res, next) => walletController.getBalance(req, res, next)));
+router.post('/history', catchAsync((req, res, next) => walletController.getHistory(req, res, next)));
+router.post('/spend', catchAsync((req, res, next) => walletController.spend(req, res, next)));
+router.post('/admin/credit', catchAsync((req, res, next) => walletController.adminCredit(req, res, next)));
 
 module.exports = router;
