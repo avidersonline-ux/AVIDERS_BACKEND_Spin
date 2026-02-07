98356ff9305c7475d2c4e763ec3a7f213dd26e52
AVIDERS <avidersonline@gmail.com>
2026-02-07 05:45:10 +0530

    Update r2.js

--- config/r2.js
+++ config/r2.js
@@ -2,14 +2,14 @@ const { S3Client } = require('@aws-sdk/client-s3');
 
 const s3Client = new S3Client({
   region: 'auto',
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: process.env.R2_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.R2_ACCESS_KEY,
+    secretAccessKey: process.env.R2_SECRET_KEY,
   },
 });
 
 module.exports = {
   s3Client,
-  bucketName: 'aviders-claims'
+  bucketName: process.env.R2_BUCKET || 'aviders-claims'
 };
